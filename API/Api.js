d10b8411e154f7dae37377d0849fc72c37e060d3
Aluno <victorsbbv_dev@hotmail.com>
2024-12-04 19:58:53 -0300

    Added API file

---
+++ API/Api.js
@@ -0,0 +1,90 @@
+const BASE_URL = 'http://localhost:5079/api/Task' // MUDAR CONFORME NOVO
+
+
+// GET
+export const getRequest = async() => {
+    try {
+        // executa o comando
+        const response = await fetch (BASE_URL, {
+            method: "GET",
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        });
+
+        if (!response.ok){
+            throw new Error(`GET Request failed with status: ${response.status}`);
+        }
+
+        const textData= await response.text();
+        const data = JSON.parse(textData);
+
+        return data;
+
+    } catch (error) {
+        
+        // tratativa - em caso de erro
+        console.error(error);
+        throw error;
+    }
+}
+
+
+// GET POR ID
+export const getRequest = async(id) => {
+    try {
+        // executa o comando
+        const response = await fetch (`${BASE_URL}/${id}`, {
+            method: "GET",
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        });
+
+        if (!response.ok){
+            throw new Error(`GET Request failed with status: ${response.status}`);
+        }
+
+        const textData= await response.text();
+        const data = JSON.parse(textData);
+
+        return data;
+
+    } catch (error) {
+        
+        // tratativa - em caso de erro
+        console.error(error);
+        throw error;
+    }
+}
+
+
+// POST
+export const postRequest = async (title, desc) => {
+    try {
+        let myBody = {
+            id : 0,
+            title: title,
+            description: desc
+        };
+
+        const response = await fetch(BASE_URL,{
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(myBody),
+        });
+
+        if(!response.ok){
+            throw new Error("Post request failed!!!")
+        }
+
+        const textData = await response.text();
+        return JSON.parse(textData);
+    } catch (error){
+        console.error(error);
+        throw error;
+}
+
+};
